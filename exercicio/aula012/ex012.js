6e7e3a77934d909c411d455db1eb49b28ac0ff76
Daniel Vasconcelos <57712464+DanielSena22@users.noreply.github.com>
2021-12-01 19:42:43 -0300

    js - aula 12

---
+++ exercicio/aula012/ex012.js
@@ -0,0 +1,9 @@
+var idade = 66
+
+if (idade < 16){
+    console.log('Não vota')
+} else if (idade < 18 || idade > 65){
+    console.log('Pode votar se quiser')
+} else{
+    console.log('Voto obrigatorio')
+}
